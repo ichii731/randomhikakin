a970466d88bf84165317558fa050c517fad82f66
tomox0115 <it0731@outlook.jp>
2021-04-09 00:30:10 +0900

    Update

--- get_video_info/a.php
+++ get_video_info/a.php
@@ -1,9 +1,20 @@
 <?php
+/*
+日時関連でエラーが出た場合は↓みたいにデフォルトタームゾーンを指定してあげてください。
 date_default_timezone_set("America/Los_Angeles");
+*/
 
-//youtube api key
-$API_KEY = "AIzaSyCawlPUFA0p4Lu1oMHqoFGYNxm6J1el1XI";
+//YouTube API v3
+$API_KEY = "***************************************";
 
+// ユーザー名からチャンネルIDを取得(ID分かってるならスキップOK)
+function get_user_channel_id($user){
+    global $API_KEY;
+    $url = 'https://www.googleapis.com/youtube/v3/channels?key=' . $API_KEY . '&part=id&forUsername=';
+    return search($user,$url)['items'][0]['id'];
+}
+
+// 
 function search($searchTerm,$url){
     $url = $url . urlencode($searchTerm);
 
@@ -16,12 +27,6 @@ function search($searchTerm,$url){
     return false;
 }
 
-function get_user_channel_id($user){
-    global $API_KEY;
-    $url = 'https://www.googleapis.com/youtube/v3/channels?key=' . $API_KEY . '&part=id&forUsername=';
-    return search($user,$url)['items'][0]['id'];
-}
-
 function push_data($searchResults){
     global $data;
     foreach($searchResults['items'] as $item){
@@ -30,39 +35,41 @@ function push_data($searchResults){
     return $data;
 }
 
-function get_url_for_utc_period($channelId, $utc){
-    //get the API_KEY
+function get_url_for_time_period($channelId, $time){
     global $API_KEY;
-    //youtube specifies the DateTime to be formatted as RFC 3339 formatted date-time value (1970-01-01T00:00:00Z)
-    $publishedAfter = date("Y-m-d\TH:i:sP",strval($utc));
-    //within a 60 day period
-    $publishedBefore_ = $utc + (60 * 60 * 24 * 60);
+    // 日時型指定
+    $publishedAfter = date("Y-m-d\TH:i:sP",strval($time));
+    //　期間を60日指定
+    $publishedBefore_ = $time + (60 * 60 * 24 * 60);
     $publishedBefore = date("Y-m-d\TH:i:sP",$publishedBefore_);
-    //develop the URL with the API_KEY, channelId, and the time period specified by publishedBefore & publishedAfter
+    // リクエスト用URL作成
     $url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&type=video&key=' . $API_KEY . '&maxResults=50&channelId=' . $channelId . '&publishedAfter=' . urlencode($publishedAfter) . '&publishedBefore=' . urlencode($publishedBefore);
 
     return array("url"=>$url,"utc"=>$publishedBefore_);
 }
-//the date that the loop will begin with, have this just before the first videos on the channel.
-//this is just an example date
-$start_date = "2011-01-01";
-$utc = strtotime($start_date);
-$username = "hikakintv";
-//get the channel id for the username
+
+
+$start_date = "YYYY-MM-DD"; // 日時指定
+$time = strtotime($start_date);
+$username = "*****";
+
 $channelId = get_user_channel_id($username);
 
-while($utc < time()){
-    $url_utc = get_url_for_utc_period($channelId, $utc);
-    $searchResults = search("", $url_utc['url']);
+while($time < time()){
+    $url = get_url_for_time_period($channelId, $time);
+    $searchResults = search("", $url['url']);
     $data = push_data($searchResults);
-    $utc += 60 * 60 * 24 * 60;
+    $time += 60 * 60 * 24 * 60; // 60日追加
 }
-print "<pre>";
-print_r($data);
-print "</pre>";
 
-//check that all of the videos have been accounted for (cross-reference this with what it says on their youtube channel)
+// 帰ってきたデータを表示するなら…
+echo "<pre>";
+var_dump($data);
+echo "</pre>";
+
+// 配列をJSON形式にして保存するなら…
+$json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+file_put_contents($username . ".json" , $json);
+
+// 動画数を出力するなら…
 print count($data);
-
-$json = json_encode($data, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
-file_put_contents("hikakin.json" , $json);
